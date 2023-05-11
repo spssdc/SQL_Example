f9767b287f30685e9f6a5511206c3dcd2ed616f2
SDC@StPaulsSchool.org.uk <SDC@StPaulsSchool.org.uk>
2023-05-11 10:10:06 +0100

    Updated querying

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using System.Collections.Generic;
 using Microsoft.Data.Sqlite;
+using System.Runtime.Versioning;
 
 namespace SQL_Example
 {
@@ -15,13 +16,37 @@ namespace SQL_Example
             connection.Open();
             var command = connection.CreateCommand();
             command.CommandText = "drop table if exists Student;";
-            command.ExecuteNonQuery();
-            command.CommandText = "CREATE TABLE Student(ID INTEGER, First TEXT, Last TEXT, Year TEXT, PRIMARY KEY(ID));";
-            command.ExecuteNonQuery();
-            command.CommandText = "INSERT INTO Student VALUES (1,'Bob','Smith','L8');";
-            command.ExecuteNonQuery();
-            command.CommandText = "INSERT INTO Student VALUES (1,'Bill','Jones','U8');";
-            command.ExecuteNonQuery();
+            command.CommandText += "CREATE TABLE Student(ID INTEGER, First TEXT, Last TEXT, Year TEXT, PRIMARY KEY(ID));";
+            command.CommandText += "INSERT INTO Student VALUES (1,'Bob','Smith','L8');";
+            command.CommandText += "INSERT INTO Student VALUES (2,'Bill','Jones','U8');";
+            connection.Close();
+
+            Console.WriteLine("Enter year to locate: ");
+            string year = Console.ReadLine();
+
+            connection.Open();
+            using (connection)
+            {
+                connection.Open();
+                command = connection.CreateCommand();
+                command.CommandText =
+                    @"
+                        SELECT First,Last
+                        FROM Student
+                        WHERE Year = $year
+                    ";
+                command.Parameters.AddWithValue("$year", year);
+
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var fname = reader.GetString(0);
+                        var lname = reader.GetString(1);
+                        Console.WriteLine(fname + " " + lname);
+                    }
+                }
+            }
             connection.Close();
         }
     }
